73f7d8e63a6a535aa0e4dadc4913d6aa9d36cf5d
koppai <psthu.koppai@gmail.com>
2023-08-28 20:18:09 +0630

    Create of_age? and can_use_services? method

--- person.rb
+++ person.rb
@@ -1,13 +1,22 @@
 class Person
-  def initialize (name = "Unknown",age,parent_permission = true)
-    @id
+  def initialize (name = "Unknown",age,parent_permission: true)
+    @id = Random.rand(1..10000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
-  attr_accessor : name
-  attr_accessor : age
-  attr_reader : id
+  attr_accessor :name, :age
+  attr_reader :id
   
-  
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
 end
