02277daeb90c4d641b156aed3fb4e8f521d30c6b
koppai <psthu.koppai@gmail.com>
2023-08-31 16:31:06 +0630

    Create create_person method

--- app.rb
+++ app.rb
@@ -2,6 +2,41 @@ class App
   def initialize
     @people = []
     @book = []
-    @rentals - []
+    @rentals = []
+  end
+
+  def choose_option
+    option = gets.chomp
+    case option
+    when "1"
+      puts "List all books"
+    when "2"
+      puts "List all people"
+    when "3"
+      create_person
+    when "4"
+      puts "Create a book"
+    when "5"
+      puts "Create a rental"
+    when "6"
+      puts "List all rentals for a given person id"
+    when "7"
+      puts "File saved successfully!"
+      puts "Thank you for using this app!"
+      exit
+    end
+  end
+
+  def create_person
+    puts "Do you want to create a student (1) or a teacher (2)? [Input the number]: "
+    input = gets.chomp
+    case input
+    when "1"
+      puts "create_student"
+    when "2"
+      puts "create_teacher"
+    else
+      puts "Please enter a valid input"
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -1,22 +1,24 @@
 require "./app"
 
+app = App.new
+
 def display_options
   puts "Welcome to school library app!
-
-Please choose an option by entering a number:
-  1 - List all books
-  2 - List all people
-  3 - Create a person
-  4 - Create a book
-  5 - Create a rental
-  6 - List all rentals for a given person id
-  7 - Exit"
+  Please choose an option by entering a number:
+    1 - List all books
+    2 - List all people
+    3 - Create a person
+    4 - Create a book
+    5 - Create a rental
+    6 - List all rentals for a given person id
+    7 - Exit"
 end
 
 def main
   loop do
     display_options
-    break
+    app.choose_option
+    puts "\n"
   end
 end
 
