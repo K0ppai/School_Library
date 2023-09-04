d9d8e4efe4d432330cb777fd96c8a89d5dc2d05a
koppai <psthu.koppai@gmail.com>
2023-09-04 17:37:03 +0630

    Remove display_options method from main.rb to app.rb

--- app.rb
+++ app.rb
@@ -12,6 +12,18 @@ class App
     @rentals = []
   end
 
+  def display_options
+    puts "Welcome to school library app!
+    Please choose an option by entering a number:
+      1 - List all books
+      2 - List all people
+      3 - Create a person
+      4 - Create a book
+      5 - Create a rental
+      6 - List all rentals for a given person id
+      7 - Exit"
+  end
+
   def choose_option
     option = gets.chomp
     case option
