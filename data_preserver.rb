57068bb0d9e59e0405611a22910f3057427091c5
koppai <psthu.koppai@gmail.com>
2023-09-05 18:41:55 +0630

    Adapt codes using SOLID principles

--- data_preserver.rb
+++ data_preserver.rb
@@ -1,5 +1,15 @@
 class DataPreserver
-  def write(file_name, data)
+  def initialize(file_name)
+    @file_name = file_name
+  end
+end
+
+class FileWriter < DataPreserver
+  def initialize(file_name)
+    super(file_name)
+  end
+  
+  def write(data)
     opts = {
       array_nl: "\n",
       object_nl: "\n",
@@ -8,6 +18,17 @@ class DataPreserver
       space: ' '
     }
     json_data = JSON.generate(data, opts)
-    File.write(file_name, json_data)
+    File.write(@file_name, json_data)
+  end
+end
+
+class FileReader < DataPreserver
+  def initialize(file_name)
+    super(file_name)
+  end
+
+  def read
+    file = File.read(@file_name) if File.exist?(@file_name)
+    JSON.parse(file)
   end
 end
