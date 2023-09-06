6b55033376629cfd30960c2111902d0c5a013f4a
koppai <psthu.koppai@gmail.com>
2023-09-06 21:55:33 +0630

    Add method tests for decorator classes

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -16,4 +16,16 @@ describe "Testing Decorator class" do
       expect(@trimmer).to be_instance_of(TrimmerDecorator)  
     end
   end
+
+  context "When using correct_name methods" do
+    it "The correct_name of CapitalizeDecorator class should return a capitalized string" do
+      capitalized_string = @capitalizer.correct_name
+      expect(capitalized_string).to eq 'Abcdefghizj'
+    end
+
+    it "The correct_name of TrimmerDecorator class should return a trimmed string" do
+      trimmed_string = @trimmer.correct_name
+      expect(trimmed_string).to eq 'abcdefghiz'
+    end
+  end
 end
