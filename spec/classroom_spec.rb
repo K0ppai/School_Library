db96e849bc47c2602c1235f3c6520573417239e1
koppai <psthu.koppai@gmail.com>
2023-09-06 20:55:08 +0630

    Add method tests in classroom_spec.rb

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -5,6 +5,7 @@ describe "something to be performed" do
   before :all do
     @classroom = Classroom.new 'A'
     @student = Student.new 12, 'Koppai'
+    @classroom.add_student(@student)
   end
   
   context "When creating classroom class" do
@@ -16,6 +17,17 @@ describe "something to be performed" do
     it "The class should have students" do
       students = @classroom.students
       expect(students).to be_instance_of Array 
+    end
+    
+    
+    it "The method add_student should add student into the student array" do
+      students = @classroom.students
+      expect(students.first).to be @student
+    end
+
+    it "The classroom of student should be the classroom class itself" do
+      student = @student
+      expect(student.classroom).to be @classroom
     end
   end
 end
