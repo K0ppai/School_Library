ca3759bcdc09976ff34559453ddf1a803d38d942
koppai <psthu.koppai@gmail.com>
2023-09-06 21:12:14 +0630

    Change describe content

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,25 +1,25 @@
-require_relative './spec_helper'
+require_relative "./spec_helper"
 
-describe "something to be performed" do
-
+describe "Testing Classroom class" do
   before :all do
-    @classroom = Classroom.new 'A'
-    @student = Student.new 12, 'Koppai'
+    @classroom = Classroom.new "A"
+    @student = Student.new 12, "Koppai"
     @classroom.add_student(@student)
   end
-  
+
   context "When creating classroom class" do
     it "The class should have label" do
       label = @classroom.label
-      expect(label).to eq "A"  
+      expect(label).to eq "A"
     end
 
     it "The class should have students" do
       students = @classroom.students
-      expect(students).to be_instance_of Array 
+      expect(students).to be_instance_of Array
     end
-    
-    
+  end
+
+  context "When implementing the methods" do
     it "The method add_student should add student into the student array" do
       students = @classroom.students
       expect(students.first).to be @student
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,42 +1,40 @@
-require './spec_helper.rb'
+require "./spec_helper.rb"
 
-
-describe Teacher do
-
+describe "Testing Teacher class" do
   before :all do
     @teacher = Teacher.new "Math", 23, "Dasy"
   end
 
   context "When creating the Teacher class" do
-
     it "The class should be a Teacher class" do
       expect(@teacher).to be_an_instance_of(Teacher)
     end
-    
 
     it "The class should have specialization" do
       specialization = @teacher.specialization
-      expect(specialization).to eq "Math"  
+      expect(specialization).to eq "Math"
     end
 
     it "The class should have age" do
       age = @teacher.age
-      expect(age).to be 23 
+      expect(age).to be 23
     end
 
     it "The class should have name" do
       name = @teacher.name
-      expect(name).to eql "Dasy"  
+      expect(name).to eql "Dasy"
     end
 
     it "The class should have parent_permission" do
       parent_permission = @teacher.parent_permission
-      expect(parent_permission).to be true 
+      expect(parent_permission).to be true
     end
+  end
 
+  context "When Implementing the methods" do
     it "The class should be able to use services" do
       can_use_services = @teacher.can_use_services?
-      expect(can_use_services).to be true 
+      expect(can_use_services).to be true
     end
   end
 end
